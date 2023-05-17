fd971e9c733374b7ef2f9cc68d60a5cccae84b24
defarrel <Defarrel>
2023-05-17 15:17:01 +0700

    Add project files.

---
+++ mbrStatic/mbrStatic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setId();
+	void printAll();
+	mahasiswa(string pnama) :nama(pnama) { setId(); }
+};
